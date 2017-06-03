1c69ce396aeeafbf9bafc2994510b0ebe9bba3b9
t.shibuta <make.app.ts@gmail.com>
2017-06-03 11:47:08 +0900

    インデントがズレてたので修正

--- webpack.config.cli.uni.js
+++ webpack.config.cli.uni.js
@@ -9,25 +9,25 @@ const app = app_utils_1.getAppFromConfig('');
 const webpackConfig = new webpack_config_1.NgCliWebpackConfig({}, app).buildConfig();
 
 module.exports = {
-    devtool: 'source-map',
-    entry: {
-        main: ['./src/cli_uni/app.server.ts', './src/cli_uni/server-aot.ts']
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    target: 'node',
-    output: {
-        path: path.join(__dirname, 'dist_universal'),
-        filename: 'server.js',
-    },
-    plugins: [
-        new ngtools.AotPlugin({
-            tsConfigPath: './tsconfig-uni.json'
-        }),
-        new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
-    ],
-    module: {
-        rules: webpackConfig.module.rules,
-    }
+  devtool: 'source-map',
+  entry: {
+    main: ['./src/cli_uni/app.server.ts', './src/cli_uni/server-aot.ts']
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'dist_universal'),
+    filename: 'server.js',
+  },
+  plugins: [
+    new ngtools.AotPlugin({
+      tsConfigPath: './tsconfig-uni.json'
+    }),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
+  ],
+  module: {
+    rules: webpackConfig.module.rules,
+  }
 }
--- webpack.config.lambda.js
+++ webpack.config.lambda.js
@@ -9,26 +9,26 @@ const app = app_utils_1.getAppFromConfig('');
 const webpackConfig = new webpack_config_1.NgCliWebpackConfig({}, app).buildConfig();
 
 module.exports = {
-    devtool: 'source-map',
-    entry: {
-        main: ['./src/lambda/app.server.ts', './src/lambda/lambda.ts']
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    target: 'node',
-    output: {
-        path: path.join(__dirname, 'dist_lambda'),
-        filename: 'lambda.js',
-        libraryTarget: 'commonjs2',
-    },
-    plugins: [
-        new ngtools.AotPlugin({
-            tsConfigPath: './tsconfig-lambda.json'
-        }),
-        // new webpack.optimize.UglifyJsPlugin({ sourceMap: true }) // aws-serverless-express でエラーが起こるので未使用
-    ],
-    module: {
-        rules: webpackConfig.module.rules,
-    }
+  devtool: 'source-map',
+  entry: {
+    main: ['./src/lambda/app.server.ts', './src/lambda/lambda.ts']
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'dist_lambda'),
+    filename: 'lambda.js',
+    libraryTarget: 'commonjs2',
+},
+  plugins: [
+    new ngtools.AotPlugin({
+      tsConfigPath: './tsconfig-lambda.json'
+    }),
+    // new webpack.optimize.UglifyJsPlugin({ sourceMap: true }) // aws-serverless-express でエラーが起こるので未使用
+],
+  module: {
+    rules: webpackConfig.module.rules,
+  }
 }
